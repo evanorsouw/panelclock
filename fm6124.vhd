dfec1cd4365d29723d75a93df5bd7aeef042f187
Eric van Orsouw <eric@whitemagic.it>
2021-03-24 21:15:25 +0100

    initial checkin

---
+++ fm6124.vhd
@@ -0,0 +1,88 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
+
+entity FM6124 is
+	port ( 	
+		clk       : in std_logic;
+		reset     : in std_logic;
+		data      : in std_logic_vector (5 downto 0);
+		--
+		addr      : out std_logic_vector (11 downto 0);
+		dsp_clk   : out std_logic;
+		dsp_latch : out std_logic;
+		dsp_oe    : out std_logic;
+		dsp_addr  : out std_logic_vector (4 downto 0);
+		dsp_r1    : out std_logic;
+		dsp_g1    : out std_logic;
+		dsp_b1    : out std_logic;
+		dsp_r2    : out std_logic;
+		dsp_g2    : out std_logic;
+		dsp_b2    : out std_logic
+	);
+end entity FM6124;
+
+architecture FM6124_arch of FM6124 is
+
+	
+begin
+	Clock_Proc: process (clk, reset)
+	variable clk_count    : unsigned (1 downto 0);
+	variable column_count : unsigned (6 downto 0);
+	variable row_count    : unsigned (4 downto 0);
+	variable pixclk       : std_logic; 
+	variable oe           : std_logic;
+	variable latch        : std_logic;
+	begin
+		if (reset = '1') then
+			clk_count    := "00";
+			column_count := "0000000";
+			row_count    := "00000";
+			pixclk       := '0';
+			latch        := '0';
+			oe           := '1';
+			
+		elsif rising_edge(clk) then
+			
+			oe        := '0';
+			clk_count := clk_count + 1;
+			
+			if (clk_count = 0) then
+				pixclk := '0';
+				
+			elsif (clk_count = 1) then			
+				dsp_r1 <= std_logic_vector(data)(0);
+				dsp_g1 <= std_logic_vector(data)(1);
+				dsp_b1 <= std_logic_vector(data)(2);
+				dsp_r2 <= std_logic_vector(data)(3);
+				dsp_g2 <= std_logic_vector(data)(4);
+				dsp_b2 <= std_logic_vector(data)(5);
+				
+			elsif (clk_count = 2) then
+				column_count := column_count + 1;
+				if (column_count = 65) then
+					latch  := '1';
+					column_count := "0000000";
+					row_count := row_count + 1;
+				else
+					latch := '0';
+					pixclk := '1';
+				end if;
+												
+			end if;			
+		end if;
+		
+		addr      <= "000000" & std_logic_vector(column_count)(5 downto 0);
+		dsp_clk   <= pixclk;
+		dsp_oe    <= oe;
+		dsp_latch <= latch;
+		dsp_addr  <= std_logic_vector(row_count);
+		
+	end process;
+	
+	
+end architecture FM6124_arch;
+				
+				
+				
+				
---
+++ ledpanel_controller.vhd
@@ -0,0 +1,113 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
+
+entity ledpanel_controller is
+	port ( 	
+		clk12M    : in std_logic;
+		reset		 : in std_logic;
+		--
+		dsp_clk   : out std_logic;
+		dsp_latch : out std_logic;
+		dsp_oe    : out std_logic;
+		dsp_addr  : out std_logic_vector (4 downto 0);
+		dsp_r1    : out std_logic;
+		dsp_g1    : out std_logic;
+		dsp_b1    : out std_logic;
+		dsp_r2    : out std_logic;
+		dsp_g2    : out std_logic;
+		dsp_b2    : out std_logic		
+	);
+end entity ledpanel_controller;
+
+architecture ledpanel_controller_arch of ledpanel_controller is
+
+	component pll
+	port (
+		inclk0	: in std_logic := '0';
+		--
+		c0			: out std_logic
+	);
+	end component;
+	
+	component FM6124
+	port ( 	
+		clk       : in std_logic;
+		reset     : in std_logic;
+		data      : in std_logic_vector (5 downto 0);
+		--
+		addr      : out std_logic_vector (11 downto 0);
+		dsp_clk   : out std_logic;
+		dsp_latch : out std_logic;
+		dsp_oe    : out std_logic;
+		dsp_addr  : out std_logic_vector (4 downto 0);
+		dsp_r1    : out std_logic;
+		dsp_g1    : out std_logic;
+		dsp_b1    : out std_logic;
+		dsp_r2    : out std_logic;
+		dsp_g2    : out std_logic;
+		dsp_b2    : out std_logic
+	);
+   end component;
+	
+	signal panel_reset : std_logic := '0';
+	signal clk         : std_logic;
+	signal ram_addr    : std_logic_vector (11 downto 0);
+	signal ram_data    : std_logic_vector (5 downto 0);
+	
+begin
+	LEDChip : FM6124
+	port map (
+		clk    => clk,
+		reset  => panel_reset,
+		data   => ram_data,
+		--
+		addr      => ram_addr,
+		dsp_clk   => dsp_clk,
+		dsp_latch => dsp_latch,
+		dsp_addr  => dsp_addr,
+		dsp_oe    => dsp_oe,
+		dsp_r1    => dsp_r1,
+		dsp_g1    => dsp_g1,
+		dsp_b1    => dsp_b1,
+		dsp_r2    => dsp_r2,
+		dsp_g2    => dsp_g2,
+		dsp_b2    => dsp_b2
+	);
+	
+	ClockGenerator : pll
+	port map (
+		inclk0  => clk12M,
+		--
+		c0 => clk
+	);
+	
+	Reset_Proc: process (clk,reset)
+	begin
+		panel_reset <= not reset;
+		if rising_edge(clk) then
+			if (to_integer(unsigned(ram_addr)) = 0) then
+				ram_data <= "000000";
+			elsif (to_integer(unsigned(ram_addr)) = 1) then
+				ram_data <= "100100";
+			elsif (to_integer(unsigned(ram_addr)) = 2) then
+				ram_data <= "010010";
+			elsif (to_integer(unsigned(ram_addr)) = 3) then
+				ram_data <= "001001";
+			elsif (to_integer(unsigned(ram_addr)) = 4) then
+				ram_data <= "111111";
+			elsif (to_integer(unsigned(ram_addr)) = 5) then
+				ram_data <= "111111";
+			elsif (to_integer(unsigned(ram_addr)) = 6) then
+				ram_data <= "111000";
+			else
+				ram_data <= "000111";
+			end if;
+		end if;
+	end process;
+	
+end architecture ledpanel_controller_arch;
+				
+				
+				
+				
