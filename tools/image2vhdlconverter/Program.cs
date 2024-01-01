1a236a14f2e0b5bc2f0d9ec846871793ba7d2f00
Eric van Orsouw <eric@whitemagic.it>
2024-01-01 17:17:37 +0100

    attempt to add boot logo

---
+++ tools/image2vhdlconverter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var image = (Bitmap)Image.FromFile("..\\..\\..\\..\\..\\fpga\\icon.png");
+        var ox = 0;
+        var oy = 0;
+
+        for (var y = 0; y < image.Height; ++y)
+        {
+            var address = 128 * (y+oy) + ox;
+            Console.Write($"X\"01\",X\"{address >> 8:X2}\",X\"{address & 0xFF:X2}\",X\"00\",X\"{image.Width:X2}\", ");
+            for (var x = 0; x < image.Width; ++x)
+            {
+                var pixel = image.GetPixel(x, y);
+                Console.Write($"X\"{pixel.R:X2}\",X\"{pixel.G:X2}\",X\"{pixel.B:X2}\", ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
